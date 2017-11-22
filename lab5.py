7009fd762762c011f46ad6a42d8a4dbc4310ca28
taqwa19-meet <taqwa19@meet.mit.edu>
2017-11-22 19:24:45 +0200

    “COMMENT”

---
+++ lab5.py
@@ -0,0 +1,20 @@
+#from turtle import *
+#colormode(255)
+#class Square (Turtle):
+   #def __init__(self,size):
+       #Turtle.__init__(self)
+        #self.shapesize(size)
+        #self.shape("square")
+
+#sss12=Square(20)
+
+class hexagon (Turtle):
+    def __init__ (self,size):
+        Turtle.__init__(self)
+        self.shapesize(size)
+        self.shape("hexagon")
+
+
+   
+        
+        
