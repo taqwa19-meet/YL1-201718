17128de1c37dd3d298f0e9305e396cb086d5d454
taqwa19-meet <taqwa19@meet.mit.edu>
2017-11-29 19:02:06 +0200

    comment

--- lab5.py
+++ lab5.py
@@ -1,20 +1,36 @@
-#from turtle import *
+from turtle import *
 #colormode(255)
+
 #class Square (Turtle):
    #def __init__(self,size):
-       #Turtle.__init__(self)
+        #Turtle.__init__(self)
         #self.shapesize(size)
         #self.shape("square")
 
 #sss12=Square(20)
 
 class hexagon (Turtle):
-    def __init__ (self,size):
+    def __init__ (self,size,speed,color):
         Turtle.__init__(self)
-        self.shapesize(size)
+        self.speed(speed)
+        self.color(color)
+        self.begin_poly()
+        self.penup()
+        for i in range(6):
+            self.fd(size)
+            self.left(60)
+        self.end_poly()
+
+        #self.shapesize(size)
+        hx2=self.get_poly()
+
+        register_shape("hexagon", hx2)
+
         self.shape("hexagon")
 
+hx = hexagon(60)
 
-   
+
+
         
         
